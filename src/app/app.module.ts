25f0f036f20108527f0bc12eb92d711f15430069
root <root@localhost.localdomain>
2018-06-22 12:51:50 +0000

    home component added and tweaked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ComponentComponent } from './component/component.component';
-import { HeaderComponent } from './header/header.component';
-import { MainTextComponent } from './main-text/main-text.component';
-import { MainButtonsComponent } from './main-buttons/main-buttons.component';
-import { SignupSectionComponent } from './signup-section/signup-section.component';
+import { HeaderComponent } from './home/header/header.component';
+import { MainTextComponent } from './home/main-text/main-text.component';
+import { MainButtonsComponent } from './home/main-buttons/main-buttons.component';
+import { SignupSectionComponent } from './home/signup-section/signup-section.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LoginComponent } from './login/login.component';
+import { ExploreComponent } from './explore/explore.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes : Routes = [
+  {path:'explore' , component: ExploreComponent},
+  {path:'about-us' , component: AboutUsComponent},
+  {path:'login' , component: LoginComponent},
+];
+
 
 @NgModule({
   declarations: [
@@ -15,10 +27,15 @@ import { SignupSectionComponent } from './signup-section/signup-section.componen
     HeaderComponent,
     MainTextComponent,
     MainButtonsComponent,
-    SignupSectionComponent
+    SignupSectionComponent,
+    AboutUsComponent,
+    LoginComponent,
+    ExploreComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
